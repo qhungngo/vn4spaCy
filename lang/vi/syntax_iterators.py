b171519d36af9f0bae063d7294261d61dda2b05a
Hung Ngo <hungnq@uit.edu.vn>
2019-07-21 11:16:30 +0100

    Update syntax_iterators.py

--- lang/vi/syntax_iterators.py
+++ lang/vi/syntax_iterators.py
@@ -12,6 +12,7 @@ def noun_chunks(obj):
         "nsubj",
         "dobj",
         "obl",
+        "obj",
         "nsubjpass",
         "pcomp",
         "compound",
@@ -30,21 +31,32 @@ def noun_chunks(obj):
     np_label = doc.vocab.strings.add("NP")
     seen = set()
     
-    #print(np_deps)
+    print("np_deps:", np_deps)
     
     for i, word in enumerate(obj):
         if word.tag_ not in ("P", "N", "Nc", "Np", "Nu"):
             continue
 
         # Prevent nested chunks from being produced
-        # print(word.i, word.dep)
         if word.i in seen:
             continue
+
+        #print(word.i, word.text, word.tag_, word.dep, conj)
         if word.dep in np_deps:
             if any(w.i in seen for w in word.subtree):
                 continue
-            seen.update(j for j in range(word.i, word.right_edge.i+1))
-            yield word.i, word.right_edge.i+1, np_label
+            i_left_edge = i_right_edge = word.i
+            while i_left_edge > word.left_edge.i:
+                if (obj[i_left_edge-1].tag_ in ["E", "R"]):
+                    break
+                i_left_edge -= 1
+            while i_right_edge < word.right_edge.i:
+                if (obj[i_right_edge+1].tag_ in ["E", "R"]):
+                    break
+                i_right_edge += 1
+            #print([obj[j].text+"/"+obj[j].tag_ for j in range(word.left_edge.i, word.right_edge.i+1)])
+            seen.update(j for j in range(i_left_edge, i_right_edge+1))
+            yield i_left_edge, i_right_edge+1, np_label
         elif word.dep == conj:
             head = word.head
             while head.dep == conj and head.head.i < head.i:
